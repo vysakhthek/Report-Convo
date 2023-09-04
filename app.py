49ae208129676127efa6d56191bf9065db8da54f
vysakhthek <vysakh.thek@gmail.com>
2023-09-05 00:06:35 +0530

    api key not loading

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 
 def get_pdf_text(pdf_files):
@@ -22,6 +24,10 @@ def get_chunks(raw_text):
     chunks = text_splitter.split_text(raw_text)
     return chunks
 
+def get_vector_store(chunks_text):
+    embdeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_texts(texts=chunks_text, embedding=embdeddings)
+    return vector_store
 
 def main():
     load_dotenv()
@@ -36,9 +42,12 @@ def main():
         if st.button("Process"):
             with st.spinner("Processing"):
                 raw_text = get_pdf_text(pdf_files)
-                st.write(raw_text)
+                # st.write(raw_text)
 
                 chunks_text = get_chunks(raw_text)
+                # st.write(chunks_text)
+
+                vector_store = get_vector_store(chunks_text)
 
 
 
